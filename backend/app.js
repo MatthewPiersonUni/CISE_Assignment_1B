5676231d049002e9c69ff89cf0ae4752bab54fee
MatthewPiersonUni <83790784+MatthewPiersonUni@users.noreply.github.com>
2022-05-13 12:48:41 +1200

    Added basic database search functionality

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,7 @@
 var express = require('express');
-var path = require('path');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -12,9 +9,30 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Move this 
+const uri = "mongodb+srv://cise:cise@mernlab.j4rip.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function databaseFind(res, collection, row, query) {
+    client.connect()
+    .then( async () => {
+        res.send(await client.db("CISE_TEST").collection(collection).find({ [row]: query }).toArray())
+    })
+}
+
+app.get('/search', (req, res) => {
+    // Get search term from 
+    var collection = req.query.collection
+    var row = req.query.row
+    var searchPhrase = req.query.search
+    console.log(collection + " " + row + " " + searchPhrase)
+    // Parse searchPhrase to prevent unwanted modifications to the database
+    // DO PARSING HERE
+    // Call databaseFind(query) to get found results back
+    databaseFind(res, collection, row, searchPhrase)
+})
+
+
 
 module.exports = app;
