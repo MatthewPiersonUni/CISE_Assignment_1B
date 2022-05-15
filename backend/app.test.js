775c97ed70287f1a3287df1508042cc507bce2bd
MatthewPiersonUni <83790784+MatthewPiersonUni@users.noreply.github.com>
2022-05-15 18:04:11 +1200

    Add comment explaining test bug

--- backend/app.test.js
+++ backend/app.test.js
@@ -3,6 +3,8 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://cise:cise@mernlab.j4rip.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// Note: This test is functional, however there is a bug where it reports as passing first before reporting a failure.
+// This won't effect CI at all as it still returns false to the OS when a test does eventually fail
 test('Test getting all articles from SPEED, should return a non-empty array.', () => {
     client.connect()
     .then(  () => {
